03e2e193377744f5172ef1ccab0582becbdd3922
Swetha Kare <swethakare41@gmail.com>
2024-09-14 16:22:48 -0400

    all new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { FlightComponent } from './pages/flight/flight.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,FlightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
